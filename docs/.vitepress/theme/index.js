16205698d1ae1bc29ff01ae5abcd0d6ff678fb85
Aleksandr Petrosyan <ap886@cantab.ac.uk>
2024-01-14 00:42:07 +0400

    [feature]: Upgrade Vitepress
    Signed-off-by: Aleksandr Petrosyan <ap886@cantab.ac.uk>

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,17 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import './style.css'
 
+/** @type {import('vitepress').Theme} */
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
 }
